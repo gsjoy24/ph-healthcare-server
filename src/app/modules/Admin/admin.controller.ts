7b4b8d3948c57542668c39d21f8b794ef5a9e5a1
gsjoy24 <gour.joy24@gmail.com>
2024-04-23 13:21:50 +0600

    added filtering

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import AdminServices from './admin.service';
 
 const getAllAdmins = async (req: Request, res: Response) => {
-	const admins = await AdminServices.getAllAdmins();
+	const admins = await AdminServices.getAllAdmins(req.query);
 	res.send(admins);
 };
 
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,8 +1,26 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-const getAllAdmins = async () => {
-	const result = await prisma.admin.findMany();
+const getAllAdmins = async (params: any) => {
+	console.log(params);
+	const result = await prisma.admin.findMany({
+		where: {
+			OR: [
+				{
+					name: {
+						contains: params?.search,
+						mode: 'insensitive' // case-insensitive search
+					}
+				},
+				{
+					email: {
+						contains: params?.search,
+						mode: 'insensitive' // case-insensitive search
+					}
+				}
+			]
+		}
+	});
 	return result;
 };
 
