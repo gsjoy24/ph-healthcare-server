cbfb23ab082b6d82cd82320ed1b433682ccd55a9
gsjoy24 <gour.joy24@gmail.com>
2024-04-23 12:54:43 +0600

    working on user and admin

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,11 +1,12 @@
 import { PrismaClient, userRole } from '@prisma/client';
-
+import bcrypt from 'bcrypt';
 const prisma = new PrismaClient();
 
 const createAdmin = async (payload: Record<string, any>) => {
+	const hashedPassword = await bcrypt.hash(payload.password, 12);
 	const userData = {
 		email: payload.admin.email,
-		password: payload.password,
+		password: hashedPassword,
 		role: userRole.ADMIN
 	};
 	const result = await prisma.$transaction(async (tx) => {
