a324d3e4ba522d2f86017ec4ad473b59d5d80af3
gsjoy24 <gour.joy24@gmail.com>
2024-04-25 18:48:06 +0600

    added catch async function

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,72 +1,57 @@
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
+import catchAsync from '../../../utils/catchAsync';
 import pick from '../../../utils/pick';
 import sendResponse from '../../../utils/sendResponse';
 import { adminFilterAbleFields } from './admin.constant';
 import AdminServices from './admin.service';
 
-const getAllAdmins = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		const params = pick(req.query, adminFilterAbleFields);
-		const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
-		const result = await AdminServices.getAllAdmins(params, options);
-		sendResponse(res, {
-			statusCode: httpStatus.OK,
-			success: true,
-			message: 'Admins fetched successfully',
-			meta: result?.meta,
-			data: result?.data
-		});
-	} catch (error: any) {
-		next(error);
-	}
-};
+const getAllAdmins = catchAsync(async (req: Request, res: Response) => {
+	const params = pick(req.query, adminFilterAbleFields);
+	const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+	const result = await AdminServices.getAllAdmins(params, options);
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		success: true,
+		message: 'Admins fetched successfully',
+		meta: result?.meta,
+		data: result?.data
+	});
+});
 
-const getAdminById = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		const { id } = req.params;
-		const result = await AdminServices.getAdminByIdFromDb(id);
+const getAdminById = catchAsync(async (req: Request, res: Response) => {
+	const { id } = req.params;
+	const result = await AdminServices.getAdminByIdFromDb(id);
 
-		sendResponse(res, {
-			statusCode: httpStatus.OK,
-			success: true,
-			message: 'Admin fetched successfully',
-			data: result
-		});
-	} catch (error: any) {
-		next(error);
-	}
-};
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		success: true,
+		message: 'Admin fetched successfully',
+		data: result
+	});
+});
 
-const updateAdmin = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		const { id } = req.params;
-		const data = req.body;
-		const result = await AdminServices.updateIntoDB(id, data);
-		sendResponse(res, {
-			statusCode: httpStatus.OK,
-			success: true,
-			message: 'Admin updated successfully',
-			data: result
-		});
-	} catch (error: any) {
-		next(error);
-	}
-};
+const updateAdmin = catchAsync(async (req: Request, res: Response) => {
+	const { id } = req.params;
+	const data = req.body;
+	const result = await AdminServices.updateIntoDB(id, data);
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		success: true,
+		message: 'Admin updated successfully',
+		data: result
+	});
+});
 
-const deleteFromDB = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		const { id } = req.params;
-		await AdminServices.deleteFromDB(id);
-		sendResponse(res, {
-			statusCode: httpStatus.OK,
-			success: true,
-			message: 'Admin deleted successfully'
-		});
-	} catch (error: any) {
-		next(error);
-	}
-};
+const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
+	const { id } = req.params;
+	await AdminServices.deleteFromDB(id);
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		success: true,
+		message: 'Admin deleted successfully'
+	});
+});
 
 const AdminControllers = {
 	getAllAdmins,
--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,23 +1,16 @@
 import { Request, Response } from 'express';
+import catchAsync from '../../../utils/catchAsync';
 import { userServices } from './user.service';
 
-const createAdmin = async (req: Request, res: Response) => {
-	try {
-		const data = req.body;
-		const result = await userServices.createAdmin(data);
-		res.status(201).json({
-			success: true,
-			message: 'Admin created successfully',
-			data: result
-		});
-	} catch (error: any) {
-		res.status(400).json({
-			success: false,
-			message: error?.name || 'Something went wrong while creating admin',
-			errors: error
-		});
-	}
-};
+const createAdmin = catchAsync(async (req: Request, res: Response) => {
+	const data = req.body;
+	const result = await userServices.createAdmin(data);
+	res.status(201).json({
+		success: true,
+		message: 'Admin created successfully',
+		data: result
+	});
+});
 
 export const userControllers = {
 	createAdmin
