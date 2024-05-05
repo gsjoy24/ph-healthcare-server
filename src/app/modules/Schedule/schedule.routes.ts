c99cedcebde9a7d58d614b19f451a97a9fbce55b
gsjoy24 <gour.joy24@gmail.com>
2024-05-05 13:41:29 +0600

    updated schema of DoctorSchedules

--- src/app/modules/Schedule/schedule.routes.ts
+++ src/app/modules/Schedule/schedule.routes.ts
@@ -1,7 +1,9 @@
+import { UserRole } from '@prisma/client';
 import express from 'express';
+import auth from '../../middlewares/auth';
 import ScheduleControllers from './schedule.controller';
 
 const router = express.Router();
 
-router.post('/', ScheduleControllers.createSchedule);
+router.post('/', auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), ScheduleControllers.createSchedule);
 export const ScheduleRoutes = router;
